a0c7a6f83aca283400e45da536146e987d831de5
Maliha-07 <malihamobasira64@gmail.com>
2025-08-10 16:21:09 +0600

    Add files via upload

---
+++ arraysum.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    int arr[100], n, sum = 0;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements: ", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+        sum += arr[i];
+    }
+
+    printf("Sum of all elements: %d\n", sum);
+    return 0;
+}
